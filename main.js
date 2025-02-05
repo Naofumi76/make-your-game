abf03a5721778a3c09df7be66f3dbad7d0a3fde7
Romain Decaux <romaindecaux.contact@gmail.com>
2025-02-05 17:01:42 +0100

    now handle levels in json file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { generateLevel } from "./internal/entities/levelGenerator.js"
+import { generateLevel, loadLevel } from "./internal/entities/levelGenerator.js"
 import { updateBallPosition, collideBallWithBricks, ball } from './internal/entities/ball.js';
 import {addScore, updateScore} from './internal/game/score.js'
 import {pauseMenu } from './internal/utils/pausemenu.js'
@@ -20,95 +20,65 @@ let paddleVelocity = 0
 
 // Add event listeners for keyboard input
 document.addEventListener('keydown', function(event) {
-    if (!isPaused) {
-        if (event.key === 'ArrowLeft') {
-            paddleVelocity -= 5
-        } else if (event.key === 'ArrowRight') {
-            paddleVelocity += 5
-        }
-    }
+	if (!isPaused) {
+		if (event.key === 'ArrowLeft') {
+			paddleVelocity -= 5
+		} else if (event.key === 'ArrowRight') {
+			paddleVelocity += 5
+		}
+	}
 })
 
 
 // Update the paddle position every frame
 function updatePaddle() {
-    let newLeft = paddle.offsetLeft + paddleVelocity
+	let newLeft = paddle.offsetLeft + paddleVelocity
 
-    // Get the game container dimensions
-    let containerWidth = gameContainer.offsetWidth
-    let paddleWidth = paddle.offsetWidth
+	// Get the game container dimensions
+	let containerWidth = gameContainer.offsetWidth
+	let paddleWidth = paddle.offsetWidth
 
-    // Check if the new position is within the game container boundaries
-    if (newLeft >= paddleWidth/2 && newLeft <= containerWidth - paddleWidth/2) {
-        paddle.style.left = newLeft + 'px'
-    }
-    paddleVelocity *= 0.8
+	// Check if the new position is within the game container boundaries
+	if (newLeft >= paddleWidth/2 && newLeft <= containerWidth - paddleWidth/2) {
+		paddle.style.left = newLeft + 'px'
+	}
+	paddleVelocity *= 0.8
 
-    requestAnimationFrame(updatePaddle)
+	requestAnimationFrame(updatePaddle)
 }
-
-
-function createBricks() {
-    const brickWidth = 50
-    const brickHeight = 20
-    const brickMargin = 0
-    const containerWidth = gameContainer.offsetWidth;
-
-    const containerHeight = gameContainer.offsetHeight;
-
-    const columns = Math.floor((containerWidth - brickMargin) / (brickWidth + brickMargin));
-
-    // Calculate the maximum number of rows
-    const paddleSpaceHeight = 5 * (brickHeight + brickMargin); // Space for 5 bricks above paddle
-    const availableHeight = containerHeight - paddleSpaceHeight;
-    const maxRows = Math.floor(availableHeight / (brickHeight + brickMargin));
-    for (let i = 0; i < maxRows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const brick = document.createElement('div')
-            brick.classList.add('brick')
-            brick.style.width = brickWidth + 'px'
-            brick.style.height = brickHeight + 'px'
-            brick.style.position = 'absolute'
-            brick.style.left = (j * (brickWidth + brickMargin) + brickMargin) + 'px'
-            brick.style.top = (i * (brickHeight + brickMargin) + brickMargin) + 'px'
-            gameContainer.appendChild(brick)
-        }
-    }
-}
-
-
 
 function createPaddle() {
-    var paddle = document.createElement('div')
-    paddle.id = 'paddle'
-    gameContainer.appendChild(paddle)
+	var paddle = document.createElement('div')
+	paddle.id = 'paddle'
+	gameContainer.appendChild(paddle)
 }
 
 function createBall() {
-    var ball = document.createElement('div')
-    ball.id = 'ball'
-    gameContainer.appendChild(ball)
+	var ball = document.createElement('div')
+	ball.id = 'ball'
+	gameContainer.appendChild(ball)
 }
 
 // Export the update function
 export function update() {
-    setGameInterval(requestAnimationFrame(update));
+	setGameInterval(requestAnimationFrame(update));
 }
 
 // Initialize the game
 function initGame() {
-    addScore()
-    addTimer()
-    timerClock()
-    if (cleanupPauseMenu) {
-        cleanupPauseMenu() // Remove old pause menu listener if it exists
-    }
-    cleanupPauseMenu = pauseMenu() // Set up new pause menu and store the cleanup function
-    paddle = document.getElementById('paddle')
-    updatePaddle()
-    updateBallPosition();
-    generateLevel(1)
-    setGameInterval(requestAnimationFrame(update));
+	addScore()
+	addTimer()
+	timerClock()
+	if (cleanupPauseMenu) {
+		cleanupPauseMenu() // Remove old pause menu listener if it exists
+	}
+	cleanupPauseMenu = pauseMenu() // Set up new pause menu and store the cleanup function
+	loadLevel(1);
+	paddle = document.getElementById('paddle')
+	updatePaddle()
+	updateBallPosition();
+	
+	setGameInterval(requestAnimationFrame(update));
 }
 
 // Call initGame after the DOM is fully loaded
