58a9266f30ccffb2d7c336e800cde4a37eabad0c
Antoine Morlay <morlayantoine@gmail.com>
2025-02-12 16:39:47 +0100

    Delete addOnelife from LevelGenerator, didnt work for some obscure reason

--- internal/entities/levelGenerator.js
+++ internal/entities/levelGenerator.js
@@ -4,7 +4,6 @@ import { isPaused, gameIsOver, setIsPaused, setGameIsOver, data } from "../utils
 import {createDialogueOverlay} from "../game/historyOverlay.js";
 import { Paddle} from "./paddle.js";
 import { Ball } from "./ball.js";
-import { addOneLife } from "./lives.js";
 
 const verbose = 1;
 export const bricks = [];
@@ -107,7 +106,6 @@ function createBrick (brick, health, brickWidth, brickHeight) {
 
 export function nextLevel(){
 	currentLevel +=1;
-	addOneLife();
 	const container = document.getElementById("gameContainer");
 	container.innerHTML = "";
 	
